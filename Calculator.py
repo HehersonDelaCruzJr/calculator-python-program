d6b2af682dedbc923aef5e9901d2055f7cab881e
Heherson Dela Cruz <delacruzheherson4477@gmail.com>
2023-05-12 16:06:10 +0800

    Ask the user if they want to try again with the operations and if they want to continue using the calculator

--- Calculator.py
+++ Calculator.py
@@ -1,49 +1,75 @@
-#Main Menu and The choices of Operations
-print("Welcome to Calculator!, Please Choose your Operation")
-print("Addition")
-print("Subtraction")
-print("Multiplication")
-print("Division")
 
-#The user will input their choice of operation
-operation = input(
-    "Enter your choice (Addition, Subtraction, Multiplication, Division): ")
-try:
-        #Getting the data from user
+while True:
+    #Main Menu and The choices of Operations
+    print("Welcome to Calculator!, Please Choose your Operation")
+    print("Addition")
+    print("Subtraction")
+    print("Multiplication")
+    print("Division")
+
+    #The user will input their choice of operation
+    operation = input("Enter your choice (Addition, Subtraction, Multiplication, Division): ")
+    try:
+        #Getting the data from user and ask the user if they want to try again with the operation
         if operation == 'Addition':
-            number1 = float(input("Please enter your first number: "))
-            number2 = float(input("Please enter your second number: "))
-            Answer = number1 + number2
-            print("Result:", Answer)
+            while True:
+                number1 = float(input("Please enter your first number: "))
+                number2 = float(input("Please enter your second number: "))
+                Answer = number1 + number2
+                print("Result:", Answer)
+                
+                try_again = input("Do you want to try again? (Y/N): ")
+                if try_again.upper() != 'Y' :
+                    break
 
         elif operation == 'Subtraction':
-            number1 = float(input("Please enter your first number: "))
-            number2 = float(input("Please enter your second number: "))
-            Answer = number1 - number2
-            print("Result:", Answer)
+            while True:
+                number1 = float(input("Please enter your first number: "))
+                number2 = float(input("Please enter your second number: "))
+                Answer = number1 - number2
+                print("Result:", Answer)
+
+                try_again = input("Do you want to try again? (Y/N): ")
+                if try_again.upper() != 'Y':
+                    break
 
         elif operation == 'Multiplication':
-            number1 = float(input("Please enter your first number: "))
-            number2 = float(input("Please enter your second number: "))
-            Answer = number1 * number2
-            print("Result:", Answer)
+            while True:
+                number1 = float(input("Please enter your first number: "))
+                number2 = float(input("Please enter your second number: "))
+                Answer = number1 * number2
+                print("Result:", Answer)
 
+                try_again = input("Do you want to try again? (Y/N): ")
+                if try_again.upper() != 'Y':
+                    break
 
         #This lets the user know that in division the denominator can't be zero
         elif operation == 'Division':
-            number1 = float(input("Please enter your first number: "))
-            number2 = float(input("Please enter your second number: "))
-            if number2 != 0:
-                Answer = number1 / number2
-                print("Result:", Answer)
-            else:
-                raise ZeroDivisionError("Cannot divide by zero")
+            while True: 
+                number1 = float(input("Please enter your first number: "))
+                number2 = float(input("Please enter your second number: "))
+                if number2 != 0:
+                    Answer = number1 / number2
+                    print("Result:", Answer)
+                else:
+                    raise ZeroDivisionError("Cannot divide by zero")
+                
+                try_again = input("Do you want to try again? (Y/N): ")
+                if try_again.upper() != 'Y':
+                        break
     
         else:
             print("Invalid input")
-except ValueError:
-        print("Invalid Number Input")
-except ZeroDivisionError as e:
-    print(str(e))
-
+    except ValueError:
+            print("Invalid Number Input")
+    except ZeroDivisionError as e:
+        print(str(e))
+        
+    #Ask the user if they want to continue if the user continue It will go back to selection of operation
+    #if the user did not continue it will print "Thank you for using Calculator" and will exit
+    try_again = input("Do you want continue? (Y/N): ")
+    if try_again.upper() != 'Y':
+        print("Thank you for using Calculator")
+        break
 
