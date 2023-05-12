913e7be8c9a9c089f12abb62f4e3874a11e3cd5f
Heherson Dela Cruz <delacruzheherson4477@gmail.com>
2023-05-12 14:29:09 +0800

    Main menu and the choices of operation that will be use

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,13 @@
+#Main Menu and The choices of Operations
+print("Welcome to Calculator!, Please Choose your Operation")
+print("Addition")
+print("Subtraction")
+print("Multiplication")
+print("Division")
 
 #Getting the data from user
-number1 = float(input("First Number:"))
-number2 = float(input("Second Number:"))
+# number1 = float(input("First Number:"))
+# number2 = float(input("Second Number:"))
 
 # Answer = number1 + number2
 # print(Answer)
@@ -13,10 +19,10 @@ number2 = float(input("Second Number:"))
 # print(Answer)
 
 #This lets the user know that in division the denominator can't be zero
-if number2 != 0:
-    Answer = number1 / number2
-    print(Answer)
+# if number2 != 0:
+#     Answer = number1 / number2
+#     print(Answer)
 
-else:
-    raise ZeroDivisionError("Cannot divide by zero")
+# else:
+#     raise ZeroDivisionError("Cannot divide by zero")
 
