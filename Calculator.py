3bcbb9e1e454057f7145aae7da47d5c1b29515f8
Heherson Dela Cruz <delacruzheherson4477@gmail.com>
2023-05-12 14:20:46 +0800

    The denaminator can't be zero in division

--- Calculator.py
+++ Calculator.py
@@ -12,5 +12,11 @@ number2 = float(input("Second Number:"))
 # Answer = number1 * number2
 # print(Answer)
 
-Answer = number1 / number2
-print(Answer)
+#This lets the user know that in division the denominator can't be zero
+if number2 != 0:
+    Answer = number1 / number2
+    print(Answer)
+
+else:
+    raise ZeroDivisionError("Cannot divide by zero")
+
